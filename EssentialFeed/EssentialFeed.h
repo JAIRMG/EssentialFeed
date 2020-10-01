eeff2bbb627d9a743d0501c0d444ca8b4b6a095b
vn0pjf7 <Jair.Moreno@walmart.com>
2020-09-30 23:40:51 -0500

    Initial Commit

---
+++ EssentialFeed/EssentialFeed.h
@@ -0,0 +1,19 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Jair Moreno G on 30/09/20.
+//  Copyright © 2020 Jair Moreno G. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
