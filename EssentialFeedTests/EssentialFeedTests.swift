ba29965c15f767591926de45ac757e2e4de91268
vn0pjf7 <Jair.Moreno@walmart.com>
2020-10-01 19:43:24 -0500

    Request data from URL on load method

--- EssentialFeedTests/EssentialFeedTests.swift
+++ EssentialFeedTests/EssentialFeedTests.swift
@@ -1,34 +1,45 @@
 //
-//  EssentialFeedTests.swift
-//  EssentialFeedTests
+//  EsentialFeedTests.swift
+//  EsentialFeedTests
 //
 //  Created by Jair Moreno G on 30/09/20.
 //  Copyright © 2020 Jair Moreno G. All rights reserved.
 //
 
 import XCTest
-@testable import EssentialFeed
 
-class EssentialFeedTests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+class RemoteFeedLoader {
+    func load() {
+        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
     }
+}
 
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
+class HTTPClient {
+    
+    static let shared = HTTPClient()
+    
+    private init() { }
+    
+    var requestedURL: URL?
+}
+
+class RemoteFeedLoaderTests: XCTestCase {
+
+    func test_init_doesNotRequestDataFromURL() {
+        let client = HTTPClient.shared
+        _ = RemoteFeedLoader()
+        
+        XCTAssertNil(client.requestedURL)
     }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
+    
+    func test_requestDataFromURL() {
+        let client = HTTPClient.shared
+        let sut = RemoteFeedLoader()
+        
+        sut.load()
+        
+        XCTAssertNotNil(client.requestedURL)
+        
     }
 
 }
