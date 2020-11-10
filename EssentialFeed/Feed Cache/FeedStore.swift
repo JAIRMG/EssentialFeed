e8685d3f72374fbbdb4d73227a285dffc5cfba6c
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2020-11-10 09:19:56 -0600

    Move LocalFeedLoader into its own production file and add access control to it

---
+++ EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,18 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Jair Moreno Gaspar on 10/11/20.
+//  Copyright © 2020 Jair Moreno G. All rights reserved.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping InsertionCompletion)
+    
+}
---
+++ EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -0,0 +1,37 @@
+//
+//  LocalFeedLoader.swift
+//  EssentialFeed
+//
+//  Created by Jair Moreno Gaspar on 10/11/20.
+//  Copyright © 2020 Jair Moreno G. All rights reserved.
+//
+
+import Foundation
+
+public final class LocalFeedLoader {
+    let store: FeedStore
+    let currentDate : () -> Date
+    
+    public init(store: FeedStore, currentDate: @escaping () -> Date) {
+        self.store = store
+        self.currentDate = currentDate
+    }
+    
+    public func save(_ items: [FeedItem], completion: @escaping (Error?) -> Void) {
+        store.deleteCachedFeed { [weak self] error in
+            guard let self = self else { return }
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
+                self.cache(items, with: completion)
+            }
+        }
+    }
+    
+    private func cache(_ items: [FeedItem], with completion: @escaping (Error?) -> Void) {
+        store.insert(items, timestamp: currentDate()) { [weak self] error in
+            guard self != nil else { return }
+            completion(error)
+        }
+    }
+}
