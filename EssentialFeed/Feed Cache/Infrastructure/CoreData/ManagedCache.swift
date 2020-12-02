3893a272402113b0b55cb864274aa13ce67ccd3e
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2020-12-01 21:38:25 -0600

    Add missing files

---
+++ EssentialFeed/Feed Cache/Infrastructure/CoreData/ManagedCache.swift
@@ -0,0 +1,30 @@
+//
+//  ManagedCache.swift
+//  EssentialFeed
+//
+//  Created by Jair Moreno Gaspar on 01/12/20.
+//  Copyright © 2020 Jair Moreno G. All rights reserved.
+//
+
+import CoreData
+
+@objc(ManagedCache)
+internal class ManagedCache: NSManagedObject {
+    @NSManaged var timestamp: Date
+    @NSManaged var feed: NSOrderedSet
+    
+    static func find(in context: NSManagedObjectContext) throws -> ManagedCache? {
+        let request = NSFetchRequest<ManagedCache>(entityName: entity().name!)
+        request.returnsObjectsAsFaults = false
+        return try context.fetch(request).first
+    }
+    
+    static func newUniqueInstance(in context: NSManagedObjectContext) throws -> ManagedCache {
+        try find(in: context).map(context.delete)
+        return ManagedCache(context: context)
+    }
+    
+    var localFeed: [LocalFeedImage] {
+        return feed.compactMap { ($0 as? ManagedFeedImage)?.local }
+    }
+}
