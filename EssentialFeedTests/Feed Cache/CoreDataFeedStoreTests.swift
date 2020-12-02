81823157c4fae04fe1ea0b9e9e42b0a64ab5f8a9
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2020-12-01 21:18:51 -0600

    Validate that delete command delivers no error on empty cache

--- EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
+++ EssentialFeedTests/Feed Cache/CoreDataFeedStoreTests.swift
@@ -66,7 +66,9 @@ class CoreDataFeedStoreTests: XCTestCase, FeedStoreSpecs {
     }
     
     func test_delete_deliversNoErrorOnEmptyCache() {
+        let sut = makeSUT()
         
+        assertThatDeleteDeliversNoErrorOnEmptyCache(on: sut)
     }
     
     func test_storeSideEffects_runSerially() {
