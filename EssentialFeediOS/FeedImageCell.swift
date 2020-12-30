9dadb9e15d88b141da1dd6a7c2ccd606fe2e44c2
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2020-12-30 13:52:35 -0600

    Render loader feed on successful load completion

---
+++ EssentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EssentialFeediOS
+//
+//  Created by Jair Moreno Gaspar on 30/12/20.
+//  Copyright © 2020 Jair Moreno G. All rights reserved.
+//
+
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
