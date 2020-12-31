6093d1b4e7ba8b62308bd5e29057e67b82819cfc
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2020-12-31 10:57:17 -0600

    Extract UIButton extension to a separate file

---
+++ EssentialFeediOSTests/Helpers/UIButton+TestHelpers.swift
@@ -0,0 +1,19 @@
+//
+//  UIButton+TestHelpers.swift
+//  EssentialFeediOSTests
+//
+//  Created by Jair Moreno Gaspar on 31/12/20.
+//  Copyright © 2020 Jair Moreno G. All rights reserved.
+//
+
+import UIKit
+
+extension UIButton {
+    func simulateTap() {
+        allTargets.forEach { target in
+            actions(forTarget: target, forControlEvent: .touchUpInside)?.forEach {
+                (target as NSObject).perform(Selector($0))
+            }
+        }
+    }
+}
