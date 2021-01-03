94104ce1188fe3dcee892bc4b61a122bf8df9db5
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-01-03 01:20:45 -0600

    Move ViewModels to separate files

---
+++ EssentialFeediOS/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  FeedViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Jair Moreno Gaspar on 03/01/21.
+//  Copyright © 2021 Jair Moreno G. All rights reserved.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
