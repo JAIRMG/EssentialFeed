3dae89b9fd8d01e53fa407fbdd4ee4ff4616983e
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-01-07 22:48:10 -0600

    Move `ErrorView` to separate class

---
+++ EssentialFeediOS/FeedUI/Views/ErrorView.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorView.swift
+//  EssentialFeediOS
+//
+//  Created by Jair Moreno Gaspar on 07/01/21.
+//  Copyright © 2021 Jair Moreno G. All rights reserved.
+//
+
+import UIKit
+
+public final class ErrorView: UIView {
+    public var message: String?
+}
