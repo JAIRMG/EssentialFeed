725560df885d3349aa5dda1483eb9595ea2ab887
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-01-07 23:11:00 -0600

    Move `FeedErrorViewModel` to its own file

---
+++ EssentialFeediOS/Feed Presentation/FeedErrorViewModel.swift
@@ -0,0 +1,11 @@
+//
+//  FeedErrorViewModel.swift
+//  EssentialFeediOS
+//
+//  Created by Jair Moreno Gaspar on 07/01/21.
+//  Copyright © 2021 Jair Moreno G. All rights reserved.
+//
+
+struct FeedErrorViewModel {
+    let message: String?
+}
