44e6c5c599f86a588bcb59f512a9781d955ba3ad
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-01-07 22:35:38 -0600

    Render error message on load feed error

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -24,7 +24,7 @@ public final class FeedUIComposer {
             feedView: FeedViewAdapter(
                 controller: feedController,
                 loader: MainQueueDispatchDecorator(decoratee: imageLoader)),
-            loadingView: WeakRefVirtualProxy(feedController))
+            loadingView: WeakRefVirtualProxy(feedController), errorView: WeakRefVirtualProxy(feedController))
  
         return feedController
     }
