b029f04dab72448bf45191e699c42de2516a25d2
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-01-08 23:56:26 -0600

    Create helper function to create SUT and track for memory leaks

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -16,13 +16,20 @@ class FeedPresenter {
 class FeedPresenterTests: XCTestCase {
     
     func test_init_doestNotSendMessagesToView() {
-        let view = ViewSpy()
-        _ = FeedPresenter(view: view)
+        let (_, view) = makeSUT()
         
         XCTAssertTrue(view.messages.isEmpty, "Expected no view messages")
     }
     
     //MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (sut: FeedPresenter, view: ViewSpy) {
+        let view = ViewSpy()
+        let sut = FeedPresenter(view: view)
+        trackForMemoryLeaks(instance: view, file: file, line: line)
+        trackForMemoryLeaks(instance: sut, file: file, line: line)
+        return (sut, view)
+    }
     
     class ViewSpy {
         let messages = [Any]()
