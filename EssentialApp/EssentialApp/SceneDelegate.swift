c765336981266748401a280374e5720eeec6acb2
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-02-15 21:52:50 -0600

    Displays cache feed on launch when user has no connectivity (due to the cache that occurs after launching with connectivity)

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -17,12 +17,49 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let _ = (scene as? UIWindowScene) else { return }
         
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
-        let session = URLSession(configuration: .ephemeral)
-        let client = URLSessionHTTPClient(session: session)
+        let client = createHTTPClient()
+        
         let remoteFeedLoader = RemoteFeedLoader(url: remoteURL, client: client)
         let remoteImageLoader = RemoteFeedImageDataLoader(client: client)
-        window?.rootViewController = FeedUIComposer.feedComposedWith(feedLoader: remoteFeedLoader,
-                                                                     imageLoader: remoteImageLoader)
+        
+        let localStoreURL = NSPersistentContainer
+            .defaultDirectoryURL()
+            .appendingPathComponent("feed-store.sqlite")
+        
+        let localStore = try! CoreDataFeedStore(storeURL: localStoreURL)
+        let localFeedLoader = LocalFeedLoader(store: localStore, currentDate: Date.init)
+        let localImageLoader = LocalFeedImageDataLoader(store: localStore)
+        
+        window?.rootViewController = FeedUIComposer.feedComposedWith(feedLoader: FeedLoaderWithFallbackComposite(
+                                                                        primary: FeedLoaderCacheDecorator(
+                                                                            decoratee: remoteFeedLoader,
+                                                                            cache: localFeedLoader),
+                                                                        fallback: localFeedLoader),
+                                                                     imageLoader: FeedImageDataLoaderWithFallBackComposite(
+                                                                        primaryLoader: FeedLoaderCacheImageDecorator(decoratee: remoteImageLoader,
+                                                                                                                     cache: localImageLoader),
+                                                                        fallbackLoader: localImageLoader))
+    }
+    
+    private func createHTTPClient() -> HTTPClient {
+        switch UserDefaults.standard.string(forKey: "connectivity") {
+            
+        case "offline":
+            return AlwaysFailingHTTPClient()
+        default:
+            return URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
+        }
     }
 }
 
+private class AlwaysFailingHTTPClient: HTTPClient {
+    
+    private class Task: HTTPClientTask {
+        func cancel() {}
+    }
+    
+    func get(from url: URL, completion: @escaping (HTTPClient.Result) -> Void) -> HTTPClientTask {
+        completion(.failure(NSError(domain: "offline app", code: 0)))
+        return Task()
+    }
+}
