3d90888f03eb902b4e2e9f429fca3d89cf662029
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-02-18 15:07:41 -0600

    Record feed with image loading snapshot (we see image with retry button)

--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -36,6 +36,14 @@ class FeedSnapshotTests: XCTestCase {
         record(snapshot: sut.snapshot(), named: "FEED_WITH_ERROR_MESSAGE")
     }
     
+    func test_feedWithFailedImageLoading() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithFailedImageLoading())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_FAILED_IMAGE_LOADING")
+    }
+    
     //MARK: - Helpers
     private func makeSUT() -> FeedViewController {
         let bundle = Bundle(for: FeedViewController.self)
@@ -61,6 +69,17 @@ class FeedSnapshotTests: XCTestCase {
                 location: "Garth Pier",
                 image: UIImage.make(withColor: .green)
             )
+        ]
+    }
+    
+    private func feedWithFailedImageLoading() -> [ImageStub] {
+        return [
+            ImageStub(description: nil,
+                      location: "Ángel de la Independencia, Mexico City",
+                      image: nil),
+            ImageStub(description: nil,
+                      location: "Obelisco, Buenos Aires",
+                      image: nil)
         ]
     }
     
