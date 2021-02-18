44768730fc54e5458bc93b1aa5ed2f8a59316854
Jair Moreno Gaspar <jair.mg.27@gmail.com>
2021-02-17 21:45:04 -0600

    Use Marks to separate properties from methods inside SceneDelegate

--- EssentialApp/EssentialApp/SceneDelegate.swift
+++ EssentialApp/EssentialApp/SceneDelegate.swift
@@ -11,6 +11,8 @@ import EssentialFeediOS
 import CoreData
 
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    // MARK: - Properties
     var window: UIWindow?
     
     private lazy var httpClient: HTTPClient = {
@@ -27,10 +29,18 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         LocalFeedLoader(store: store, currentDate: Date.init)
     }()
     
+    // MARK: - Init
+    
     convenience init(httpClient: HTTPClient, store: FeedStore & FeedImageDataStore) {
         self.init()
         self.httpClient = httpClient
         self.store = store
+    }
+    
+    // MARK: - Methods
+
+    func sceneWillResignActive(_ scene: UIScene) {
+        localFeedLoader.validateCache { _ in }
     }
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
@@ -38,7 +48,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         
         configureWindow()
     }
-    
+
     func configureWindow() {
         
         let remoteURL = URL(string: "https://static1.squarespace.com/static/5891c5b8d1758ec68ef5dbc2/t/5db4155a4fbade21d17ecd28/1572083034355/essential_app_feed.json")!
@@ -57,9 +67,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
                                                                         primaryLoader: FeedLoaderCacheImageDecorator(decoratee: remoteImageLoader,
                                                                                                                      cache: localImageLoader),
                                                                         fallbackLoader: localImageLoader)))
-    }
-    
-    func sceneWillResignActive(_ scene: UIScene) {
-        localFeedLoader.validateCache { _ in }
     }
 }
